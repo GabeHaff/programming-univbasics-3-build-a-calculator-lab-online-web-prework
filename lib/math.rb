01dfb98495331d835643968e0c1305f80cb22ef8
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2019-08-30 16:16:45 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,16 +1,25 @@
 def addition(num1, num2)
-
+num1=5
+num2=6
+addition puts num1+num2 
 end
 
 def subtraction(num1, num2)
-
+num1=2 
+num2=1
+subtraction puts num1-num2
 end
 
 def division(num1, num2)
-
+num1=6
+num2=3
+division puts num1/num2 
 end
 
 def multiplication(num1, num2)
+num1=2 
+num2= 6 
+multiplication puts num1*num2 
 
 end
 
