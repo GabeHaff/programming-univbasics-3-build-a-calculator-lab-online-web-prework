1140a12a294d49320d3bfb156cecec85c47ff76b
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2019-08-30 16:19:58 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,19 +1,19 @@
 def addition(num1, num2)
 num1=5
 num2=6
-addition puts num1+num2 
+return num1+num2 
 end
 
 def subtraction(num1, num2)
 num1=2 
 num2=1
-subtraction puts num1-num2
+return num1-num2
 end
 
 def division(num1, num2)
 num1=6
 num2=3
-division puts num1/num2 
+return num1/num2 
 end
 
 def multiplication(num1, num2)
@@ -24,7 +24,7 @@ return num1*num2
 end
 
 def modulo(num1, num2)
-
+num1=
 end
 
 def square_root(num)
